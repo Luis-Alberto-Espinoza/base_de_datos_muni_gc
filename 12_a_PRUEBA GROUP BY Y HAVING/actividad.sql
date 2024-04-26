e41aa079c86953ed530e50f68601a9d117fef159
Luis-Alberto-Espinoza <espinoza.luis.alberto1981@gmail.com>
2024-04-26 18:03:17 -0300

    ordenando tareas

---
+++ 12_a_PRUEBA GROUP BY Y HAVING/actividad.sql
@@ -0,0 +1,21 @@
+-- # ¿Ponemos a prueba lo aprendido utilizando la base Musimundos?
+
+-- ## Base de Datos Musimundos
+
+
+-- El área de estrategia de Musimundos está analizando cómo implementar una acción de marketing, en la cual tiene que destinar un presupuesto acorde a la cantidad de clientes que tenga cada país. Por esto te piden que los ayudemos a identificar la cantidad de clientes que son de Brazil. Seleccioná el número que obtuviste.
+-- opciones(05, 02, 10, 07)
+
+-- En Musimundos estamos muy atentos a que nuestros usuarios encuentren una cantidad apropiada de canciones del estilo que quieran escuchar. Es por eso que en esta oportunidad nos están solicitando una lista de cuantas canciones tenemos por género. Para esto, hacé una consulta a la base de datos que traiga el id del género y la cantidad de canciones que posee. Seleccioná el número que te dio como resultado de la cantidad de canciones del género con id = 6.
+-- opciones(332, 48, 81, 01)
+
+-- Es época de balances, y en Musimundos queremos saber cuánto dinero llevan gastados los clientes en nuestra empresa. Seleccioná el número que obtuviste. 
+-- opciones(2130.60, 4855.60, 2328.60, 2330.60)
+-- En el sitio web de Musimundos, queremos informar la duración promedio en milisegundos de cada álbum. Obtiene una lista de todos los álbumes y su duración (milisegundos) promedio. Inserta el número del promedio que obtuviste para el álbum "Let There Be Rock"
+-- opciones(298517.1050, 326547.9050, 306657.3750, 305547.6050)
+
+-- En el equipo de desarrollo están mudando de servidor a nuestra aplicación, y quieren saber cuál es el peso (bytes) de cada archivo de canciones para buscar un servidor adecuado con capacidad suficiente. Hace una consulta a la base de datos para saber cuál es el archivo con menor peso en bytes. Seleccioná el número que obtuviste.
+-- opciones(32717, 36787, 38747, 30727)
+
+-- En el salón de Musimundos, estamos ofreciendo un descuento para todos aquellos clientes que tengan un total de facturación mayor a 45. Hacé una consulta a la base de datos que nos traiga por id_cliente la suma total de sus facturas. Filtra aquellos que la suma del total sea 45. ¿Cuál es la suma total del cliente con id = 6? Por último, seleccioná el número que obtuviste.
+-- opciones(43.12, 50.62, 49.62, 47.72)
---
+++ 13_SUBQUERIES_TABLE REFERENCE/avtividades_resueltas.sql
@@ -0,0 +1,10 @@
+-- # Consultas
+-- ## SUBQUERIES Y TABLE REFERENCE
+
+-- Utilizando la base de datos de movies, queremos conocer, por un lado, los títulos y el nombre del género de todas las series de la base de datos.
+
+-- Por otro, necesitamos listar los títulos de los episodios junto con el nombre y apellido de los actores que trabajan en cada uno de ellos.
+
+-- Para nuestro próximo desafío, necesitamos obtener a todos los actores o actrices (mostrar nombre y apellido) que han trabajado en cualquier película de la saga de La Guerra de las galaxias.
+
+-- Crear un listado a partir de la tabla de películas, mostrar un reporte de la cantidad de películas por nombre de género.
